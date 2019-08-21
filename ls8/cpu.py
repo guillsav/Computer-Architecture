e365cbb6f590ad8f9ab3c648209e1b272568670f
guillsav <guillsav@gmail.com>
2019-08-21 19:01:19 -0400

    Day 2 MVP done

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -30,31 +30,53 @@ class CPU:
 
     def load(self):
         """Load a program into memory."""
-
         address = 0
+
+        if len(sys.argv) != 2:
+            print(f"Usage: {sys.argv[0]} <filename>", file=sys.stderr)
+            sys.exit(1)
+        else:
+            filepath = sys.argv[1]
+            try:
+                with open(filepath) as f:
+                    for line in f:
+                        # Split before and after comment symbols.
+                        comment_split = line.split("#")
+                        # Strip the extra space on each line.
+                        num = comment_split[0].strip()
+                        if num == "":
+                            continue
+                        x = int(num, 2)
+                        self.ram[address] = x
+                        address += 1
+
+            except FileNotFoundError:
+                print(f"{sys.argv[0]}: {sys.argv[1]} not found")
+                sys.exit(2)
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010,  # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111,  # PRN R0
-            0b00000000,
-            0b00000001,  # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010,  # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111,  # PRN R0
+        #     0b00000000,
+        #     0b00000001,  # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        # elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -84,6 +106,7 @@ class CPU:
         LDI = 0b10000010
         PRN = 0b01000111
         HLT = 0b00000001
+        MUL = 0b10100010
 
         running = True
 
@@ -100,6 +123,9 @@ class CPU:
             elif IR == PRN:
                 print(self.reg[operand_a])
                 self.pc += 2
+            elif IR == MUL:
+                self.alu('MUL', operand_a, operand_b)
+                self.pc += 3
             elif IR == HLT:
                 running = False
             else:
